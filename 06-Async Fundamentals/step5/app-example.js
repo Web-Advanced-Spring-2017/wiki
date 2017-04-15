a15816db8e9dc0792ff7dbad85be80c39690fa5d
Jaskirat <jaskiratr@gmail.com>
2017-04-15 00:49:54 -0400

    formatting

--- 06-Async Fundamentals/step5/app-example.js
+++ 06-Async Fundamentals/step5/app-example.js
@@ -1,59 +1,59 @@
 // Examples from : https://alexperry.io/node/2015/03/25/promises-in-node.html
 
-//Pseudo code
+// Pseudo code
 // fs module does not return promises.
-const fs = require('fs');
-/*
-//Just another callback hell
+const fs = require('fs')
+	/*
+	//Just another callback hell
 
-fs.readFile('directory/file-to-read', function(err, file) {
-  if (error) {
-    //handle error
-  } else {
-    //do something with the file
-    fs.mkdir('directory/new-directory', function(err, file) {
-      if (error) {
-        //handle error
-      } else {
-        //new directory has been made
-        fs.writeFile('directory/new-directory/message.txt', function(err, file) {
-          if (error) {
-            // handle error
-          } else {
-            // File successfully created
-          }
-        });
-      }
-    });
-  }
-});
-*/
+	fs.readFile('directory/file-to-read', function(err, file) {
+		if (error) {
+			//handle error
+		} else {
+			//do something with the file
+			fs.mkdir('directory/new-directory', function(err, file) {
+				if (error) {
+					//handle error
+				} else {
+					//new directory has been made
+					fs.writeFile('directory/new-directory/message.txt', function(err, file) {
+						if (error) {
+							// handle error
+						} else {
+							// File successfully created
+						}
+					});
+				}
+			});
+		}
+	});
+	*/
 
-//-----------------------------------------------
+// -----------------------------------------------
 // Chaining
-//-----------------------------------------------
+// -----------------------------------------------
 fs.readFileAsync('directory/file-to-read')
-  .then((fileData) => {
-    return fs.mkdirAsync('directory/new-directory');
-  })
-  .then(() => {
-    return fs.writeFileAsync('directory/new-directory/message.txt');
-  }).catch((error) => {
-    //do something with the error and handle it
-  });
+	.then((fileData) => {
+		return fs.mkdirAsync('directory/new-directory')
+	})
+	.then(() => {
+		return fs.writeFileAsync('directory/new-directory/message.txt')
+	}).catch((error) => {
+		// do something with the error and handle it
+	})
 
-//-----------------------------------------------
+// -----------------------------------------------
 // Modularise
-//-----------------------------------------------
+// -----------------------------------------------
 function readFileandMakeDirectory() {
-  return fs.readFileAsync('directory/file-to-read')
-    .then((fileData) => {
-      return fs.mkdirAsync('directory/new-directory');
-    });
+	return fs.readFileAsync('directory/file-to-read')
+		.then((fileData) => {
+			return fs.mkdirAsync('directory/new-directory')
+		})
 }
 
-//The following will execute once the file has been read and a new directory has been made
+// The following will execute once the file has been read and a new directory has been made
 readFileandMakeDirectory()
-  .then(() => {
-    return fs.writeFileAsync('directory/new-directory/message.txt');
-  });
+	.then(() => {
+		return fs.writeFileAsync('directory/new-directory/message.txt')
+	})
